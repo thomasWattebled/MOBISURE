a2f10cb1f867be5d879fbd7584b443012dd88878
thomas <thomas.wattebled2.etu>
2024-12-31 13:05:20 +0100

    ajout de toutes les infos pour la bdd

--- project/mobisure_front/src/components/register/RegistrationForm.js
+++ project/mobisure_front/src/components/register/RegistrationForm.js
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
+import RegistrationFormView from './RegistrationFormView';
 
 const RegistrationForm = () => {
   const [formData, setFormData] = useState({
     nom: '',
-	prenom: '',
+	  prenom: '',
     mail: '',
     mdp: '',
+    sexe: '',
+    dateNaissance: '',
+    adresse: '', 
+    telephone: '',
   });
 
   const handleChange = (e) => {
@@ -15,13 +20,17 @@ const RegistrationForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-  
+
+    const formDataWithDate = {
+      ...formData,
+      date_creation: new Date().toISOString().split('T')[0], // Ajout date de la creation du compte pour la bdd
+    };
     fetch('http://localhost:8080/users/register', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(formData),
+      body: JSON.stringify(formDataWithDate),
     })
       .then((response) => response.text())
       .then((data) => {
@@ -34,82 +43,11 @@ const RegistrationForm = () => {
   
 
   return (
-    <div className="container mt-5">
-      <div className="row justify-content-center">
-        <div className="col-md-6">
-          <div className="card">
-            <div className="card-body">
-              <h2 className="card-title text-center">Inscription</h2>
-              <form onSubmit={handleSubmit}>
-                <div className="mb-3">
-                  <label htmlFor="nom" className="form-label">
-                    Nom :
-                  </label>
-                  <input
-                    type="text"
-                    id="nom"
-                    name="nom"
-                    value={formData.nom}
-                    onChange={handleChange}
-                    className="form-control"
-                    placeholder="Entrez votre nom"
-                    required
-                  />
-                </div>
-				<div className="mb-3">
-					<label htmlFor="prenom" className="form-label">
-					Prénom :
-					</label>
-					<input
-						type="text"
-						id="prenom"
-						name="prenom"
-						value={formData.prenom}
-						onChange={handleChange}
-						className="form-control"
-						placeholder="Entrez votre prénom"
-						required
-						/>
-				</div>
-                <div className="mb-3">
-                  <label htmlFor="mail" className="form-label">
-                    mail :
-                  </label>
-                  <input
-                    type="mail"
-                    id="mail"
-                    name="mail"
-                    value={formData.mail}
-                    onChange={handleChange}
-                    className="form-control"
-                    placeholder="Entrez votre mail"
-                    required
-                  />
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="mdp" className="form-label">
-                    Mot de passe :
-                  </label>
-                  <input
-                    type="password"
-                    id="mdp"
-                    name="mdp"
-                    value={formData.mdp}
-                    onChange={handleChange}
-                    className="form-control"
-                    placeholder="Entrez votre mot de passe"
-                    required
-                  />
-                </div>
-                <button type="submit" className="btn btn-primary w-100">
-                  S'inscrire
-                </button>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <RegistrationFormView
+      formData={formData}
+      handleChange={handleChange}
+      handleSubmit={handleSubmit}
+    />
   );
 };
 
