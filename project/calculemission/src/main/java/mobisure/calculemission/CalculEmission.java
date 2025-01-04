f830e179a768f3d8aabffe1e825dd266be062659
maxime.de-sainte-maresville.etu <maxime.de-sainte-maresville.etu@univ-lille.fr>
2025-01-04 21:20:54 +0100

    webservice pour emission de co2 à partir de deux coordonnées gps et d'un moyen de stransport

--- project/calculemission/src/main/java/mobisure/calculemission/CalculEmission.java
+++ project/calculemission/src/main/java/mobisure/calculemission/CalculEmission.java
@@ -1,15 +1,21 @@
 package mobisure.calculemission;
 
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
+
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 
 /**
  * Root resource (exposed at "myresource" path)
  */
-@Path("myresource")
-public class MyResource {
+@Path("calculemission")
+public class CalculEmission {
+	
+	private ImpactCOD impactCOD = new ImpactCOD();
+	private OpenRouteService openRouteService = new OpenRouteService("5b3ce3597851110001cf6248f4370619d17745c38e112fff99705c37");
 
     /**
      * Method handling HTTP GET requests. The returned object will be sent
@@ -18,8 +24,18 @@ public class MyResource {
      * @return String that will be returned as a text/plain response.
      */
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getIt() {
-        return "Got it!";
+    @Path("/getemission")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public double getEmission(@QueryParam("gpsStart") String gpsStart,
+    		@QueryParam("gpsEnd") String gpsEnd,
+    		@QueryParam("transport") int transport){
+    	double emission = -1.0;
+    	
+    	double distance = this.openRouteService.getDistance(gpsStart, gpsEnd);
+    	
+    	emission = this.impactCOD.getEmission(transport, distance);
+    	
+        return emission;
     }
 }
