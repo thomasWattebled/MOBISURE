bf91b7eab2ecca7206d0d945cad4b2109f9b360f
Benjamin <benjaminalexandremail@gmail.com>
2025-01-16 12:14:18 +0100

    Ajout des tests pour UserRoleUpdateRequest

---
+++ project/Back/src/test/java/mobisure/project/request/UserRoleUpdateRequestTest.java
@@ -0,0 +1,42 @@
+package mobisure.project.request;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class UserRoleUpdateRequestTest {
+
+	private UserRoleUpdateRequest request;
+	
+	@BeforeEach
+	public void setUp() {
+		
+		List<String> roles = new ArrayList<>();
+		
+		request = new UserRoleUpdateRequest();
+		request.setUserId((long) 1);
+		request.setRoles(roles);
+	}
+	
+	@Test
+	void testUserId() {
+		assertEquals((long) 1,request.getUserId());
+		request.setUserId((long)2);
+		assertEquals((long) 2,request.getUserId());
+	}
+	
+	@Test
+	void testRoles() {
+		List<String> roles = new ArrayList<>();
+		assertEquals(roles,request.getRoles());
+		roles.add("Role1");
+		request.setRoles(roles);
+		assertEquals(roles,request.getRoles());
+		assertTrue(request.getRoles().contains("Role1"));
+	}
+}
