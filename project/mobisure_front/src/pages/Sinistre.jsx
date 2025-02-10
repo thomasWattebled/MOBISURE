1fed4ce45e27d3f12b1e66c9247c8a32d5d34480
thomas <thomas.wattebled2.etu>
2025-02-10 12:01:18 +0100

    debut sinistre

---
+++ project/mobisure_front/src/pages/Sinistre.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import AddSinistre from '../components/sinistre/addSinistre';
+
+const Sinistre = () => (
+  <div className="container mt-5">
+    <h1>Gestion des sinistre</h1>
+    <AddSinistre/>
+  </div>
+);
+
+export default Sinistre;
