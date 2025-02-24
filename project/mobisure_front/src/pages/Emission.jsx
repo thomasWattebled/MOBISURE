e59f380eea8b622b39709d7253b9ac90fc768e69
maxime.de-sainte-maresville.etu <maxime.de-sainte-maresville.etu@univ-lille.fr>
2025-02-24 11:06:14 +0100

    ajout de page emissions

---
+++ project/mobisure_front/src/pages/Emission.jsx
@@ -0,0 +1,90 @@
+import React, {useState} from 'react';
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet'
+import markerIconPng from "leaflet/dist/images/marker-icon.png"
+import {Icon} from 'leaflet'
+import 'leaflet/dist/leaflet.css';
+import CoordinateInput from '../components/emission/CoordinateInput'
+import coordinateToAddressInstance from '../services/CoordinateToAddressService';
+import CalculateEmission from '../components/emission/CalculateEmission';
+import TransportSelection from '../components/emission/TransportSelection';
+
+const Emission = () => {
+    const [clickPosition, setClickPosition] = useState('');
+    const position = [51.505, -0.09];
+
+    const [startCoordinates, setStartCoordinates] = useState('8.681495,49.41461');
+    const [endCoordinates, setEndCoordinates] = useState('8.687872,49.420318');
+
+    const [startValue, setStartValue] = useState('');
+    const [endValue, setEndValue] = useState('');
+
+    const [startFocused, setStartFocused] = useState(false);
+    const [endFocused, setEndFocused] = useState(false);
+
+    const [transport, setTransport] = useState(4);
+
+    const handleStartFocused = () => {
+        setStartFocused(true);
+        setEndFocused(false);
+    }
+
+    const handleEndFocused = () => {
+        setEndFocused(true);
+        setStartFocused(false);
+    }
+
+    const handleCoordinatesChange = (e) => {
+        if (startFocused) coordinateToAddressInstance.translateToAddress(e.lat, e.lng).then((value) => {
+            setStartValue(value);
+            setStartCoordinates(e.lng + "," + e.lat);
+        });
+        if (endFocused) coordinateToAddressInstance.translateToAddress(e.lat, e.lng).then((value) => {
+            setEndValue(value);
+            setEndCoordinates(e.lng + "," + e.lat);
+        });
+    }
+
+    function MapEvents() {
+        useMapEvents({
+            click(e) {
+                setClickPosition(e.latlng);
+                handleCoordinatesChange(e.latlng);
+            },
+        });
+        return null;
+    }
+
+    return (
+        <>
+            <MapContainer 
+                center={position}
+                zoom={10} scrollWheelZoom={false} 
+                style={{ height: '400px', width: '100%' }}
+            >
+                <TileLayer
+                    attribution='Map'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                {clickPosition && (
+                    <Marker
+                        position={clickPosition}
+                        icon={new Icon({iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41]})}
+                    >
+                        <Popup>
+                            Coordonnées du clic : <br />
+                            Latitude: {clickPosition.lat} <br />
+                            Longitude: {clickPosition.lng}
+                        </Popup>
+                    </Marker>
+                )}
+                <MapEvents />
+            </MapContainer>
+            <CoordinateInput coordinateName="Départ" coordinateValue={startValue} isFocusedFunct={handleStartFocused} isFocused={startFocused}/>
+            <CoordinateInput coordinateName="Arrivée" coordinateValue={endValue} isFocusedFunct={handleEndFocused} isFocused={endFocused}/>
+            <TransportSelection transport={transport} setTransport={setTransport}/>
+            <CalculateEmission startCoordonate={startCoordinates} endCoordonate={endCoordinates} transport={transport} />
+        </>
+    )
+}
+
+export default Emission;
